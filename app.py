d1f54d9e655c4776a9350846b225cb6766eee9a3
iqbalramad1211 <iqbalramad1211@gmail.com>
2023-12-07 15:36:48 +0700

    SecondCommit

--- app.py
+++ app.py
@@ -36,6 +36,19 @@ def home():
         return redirect(url_for('login', msg="Your login token has expired"))
     except jwt.exceptions.DecodeError:
         return redirect(url_for('login', msg="There was an error logging you in"))
+    
+@app.route('/pegawai' , methods=['GET', 'POST'])
+def home_pegawai():
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload['id']})
+        return render_template('pegawai.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for('login', msg="Your login token has expired"))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for('login', msg="There was an error logging you in"))
+
 
 
 @app.route('/login', methods=['GET'])
@@ -142,6 +155,81 @@ def sign_in():
                 "msg": "We could not find a user with that id/password combination",
             }
         )
+
+
+@app.route("/profile")
+def profile():
+    token_receive = request.cookies.get(TOKEN_KEY)
+    print("Received Token:", token_receive)
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print("Decoded Payload:", payload)
+        user_info = db.users.find_one({"username": payload['id']}, {'_id': False, 'password': False})
+        return render_template('profile.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        print("Expired Signature Error")
+        return jsonify({'result': 'fail', 'msg': 'Your login token has expired'})
+    except jwt.exceptions.DecodeError:
+        print("Decode Error")
+        return jsonify({'result': 'fail', 'msg': 'There was an error decoding your token'})
+
+
+@app.route('/get_profile/profile', methods=['GET'])
+def get_profile():
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload['id']}, {'_id': False, 'password': False})
+        name = payload.get('name', '')
+        nik = payload.get('nik', '')
+        tgl_lahir = payload.get('tgl_lahir', '')
+        gender = payload.get('gender', '')
+        agama = payload.get('agama', '')
+        status = payload.get('status', '')
+        alamat = payload.get('alamat', '')
+        no_telp = payload.get('no_telp', '')
+        db.users.update_one(
+            {'username': username},
+            {'$set': {'name': name, 'nik': nik, 'tgl_lahir': tgl_lahir, 'gender': gender,
+                      'agama': agama, 'status': status, 'alamat': alamat, 'no_telp': no_telp}}
+        )
+        return jsonify({'result': 'success', 'user_info': user_info})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': 'Your login token has expired'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': 'There was an error decoding your token'})
+
+
+
+@app.route('/profile/edit', methods=['POST'])
+def edit_profile():
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload['id']
+
+        # Fetch user data from the request
+        name = request.form.get('name')
+        nik = request.form.get('nik')
+        tgl_lahir = request.form.get('tgl-lahir')
+        gender = request.form.get('gender')
+        agama = request.form.get('agama')
+        status = request.form.get('status')
+        alamat = request.form.get('alamat')
+        no_telp = request.form.get('no-telp')
+
+        # Update user data in the database
+        db.users.update_one(
+            {'username': username},
+            {'$set': {'name': name, 'nik': nik, 'tgl_lahir': tgl_lahir, 'gender': gender,
+                      'agama': agama, 'status': status, 'alamat': alamat, 'no_telp': no_telp}}
+        )
+
+        return jsonify({'result': 'success', 'msg': 'Profile updated successfully'})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': 'Your login token has expired'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': 'There was an error decoding your token'})
 
 
 if __name__ == '__main__':
